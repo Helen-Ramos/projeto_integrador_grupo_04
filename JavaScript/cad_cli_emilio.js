05ca60f61761ac88c067dc18dc56ade763c8c67d
Emilio Barbosa <117140288+3mil10@users.noreply.github.com>
2023-11-26 08:38:14 -0300

    Validação do cadastro de clientes

--- JavaScript/cad_cli_emilio.js
+++ JavaScript/cad_cli_emilio.js
@@ -1,15 +1,14 @@
 const form = document.getElementById('form');
 const campos = document.querySelectorAll('.required');
 const span = document.querySelectorAll('.span-required');
-const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
+const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
-form.addEventListener('submit', (event) =>{
+form.addEventListener('submit', (event) => {
     event.preventDefault();
     emailValidate();
     mainPasswordValidate();
-    comparePassword()
+    comparePassword();
 });
-
 
 //  validar campo  //
 function setError(index) {
@@ -25,34 +24,24 @@ function removeError(index) {
 function emailValidate() {
     if (!emailRegex.test(campos[0].value)) {
         setError(0);
-    }
-    else {
+    } else {
         removeError(0);
-        
     }
 }
 
-function mainPasswordValidate()
-{
-    if (campos[1].value.length < 8){
+function mainPasswordValidate() {
+    if (campos[1].value.length < 8) {
         setError(1);
-    }
-    else{
+    } else {
         removeError(1);
         comparePassword();
     }
-}   
+}
 
-
-function comparePassword(){
-    if (campos[1].value == campo[2].value && campo[2].value.length < 8)
-    {
+function comparePassword() {
+    if (campos[1].value === campos[2].value && campos[2].value.length >= 8) {
         removeError(2);
-    }
-    else{
+    } else {
         setError(2);
     }
 }
-
-
-
